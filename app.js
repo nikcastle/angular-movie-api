05fd5797602902cb6864a8b991f015edfc35f9fc
Lady Stock <nikki.castlea@gmail.com>
2020-11-21 08:00:33 -0700

    api function added

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    $scope.names = [{
-        firstName: "testing1",
-        lastName: "tester1"
-    }, {
-        firstName: "testing2",
-        lastName: "tester2"
-    }, {
-        firstName: "testing3",
-        lastName: "tester3"
-    }]
-
-    $scope.myFunction = function () {
-        $scope.firstName = "test";
-        $scope.lastName = "testing"
+app.controller('myCtrl', function ($scope, $http) {
+    
+    $scope.movieSearch = function(movieTitle){
+        $scope.movieTitle = "search"
+        
     }
+     
+    // $scope.firstName = "John";
+    // $scope.lastName = "Doe";
+    // $scope.names = [{
+        //     firstName: "testing1",
+        //     lastName: "tester1"
+        // }, {
+            //     firstName: "testing2",
+            //     lastName: "tester2"
+            // }, {
+                //     firstName: "testing3",
+                //     lastName: "tester3"
+                // }]
+                
+                // $scope.myFunction = function () {
+                    //     $scope.firstName = "test";
+                    //     $scope.lastName = "testing"
+                    // }
+                    
+    $scope.apiCall = function(movieTitle) {
 
-    $scope.apiCall = function() {
-        $http("URL").then(function(result){
+        $http({
+            method: "GET",
+            url: "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=24dbfc1d&"})
+            .then(function(result){
             $scope.names = result.data
+            console.log(result.data)
+            console.log(movieTitle);
+            ;
+            
         })
     }
 
-    $scope.myFunction();
+    // $scope.myFunction();
 });
