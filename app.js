387bf72a670f60f79853e6018201f0beb53b090d
Lady Stock <nikki.castlea@gmail.com>
2020-11-21 09:28:16 -0700

    working through related search results bug

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     
-    $scope.movieSearch = function(movieTitle){
-        $scope.movieTitle = "search"
+    // $scope.movieSearch = function(movieTitle){
+    //     $scope.movieTitle = "search"
         
-    }
-     
-    // $scope.firstName = "John";
-    // $scope.lastName = "Doe";
-    // $scope.names = [{
-        //     firstName: "testing1",
-        //     lastName: "tester1"
-        // }, {
-            //     firstName: "testing2",
-            //     lastName: "tester2"
-            // }, {
-                //     firstName: "testing3",
-                //     lastName: "tester3"
-                // }]
-                
-                // $scope.myFunction = function () {
-                    //     $scope.firstName = "test";
-                    //     $scope.lastName = "testing"
-                    // }
+    // }
                     
     $scope.apiCall = function(movieTitle) {
         // Search for a single movie
-        $http.get("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=24dbfc1d&")
+        $http.get("http://www.omdbapi.com/?t=" + movieTitle + "&plot=full&apikey=24dbfc1d&")
             .then(function(result){
             $scope.details = result.data
             console.log(result.data)
         })
+
         // Search for related titles
         $http.get("http://www.omdbapi.com/?s=" + movieTitle + "&apikey=24dbfc1d&")
         .then(function(result) {
