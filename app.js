fd4e8b58ae2f3f3673412eec65785e5a36ca0126
Lady Stock <nikki.castlea@gmail.com>
2020-11-22 11:42:38 -0700

    set up if statement in case of no year for advanced search

--- app.js
+++ app.js
@@ -35,11 +35,16 @@ app.controller('myCtrl', function ($scope, $http) {
 
     // Advanced search using more parameters
     $scope.advanced = function(user) {
-        console.log("clicked");
-        console.log(user)
+        var url;
         var title = user.title.split(" ").join("+")
+        
+        if(user.year) {
+            url = "http://www.omdbapi.com/?t=" + title + "&type=" + user.select + "&y=" + user.year +"&plot=full&apikey=24dbfc1d&"
+        } else {
+            url = "http://www.omdbapi.com/?t=" + title + "&type=" + user.select + "&plot=full&apikey=24dbfc1d&"
+        }
 
-        var url = "http://www.omdbapi.com/?t=" + title + "&type=" + user.select + "&y=" + user.year +"&plot=full&apikey=24dbfc1d&"
+        console.log(user)
 
         console.log(url);
 
@@ -47,7 +52,8 @@ app.controller('myCtrl', function ($scope, $http) {
             .then(function(result){
             $scope.details = result.data
             console.log(result.data)
-        })
+        })  
+
     }
 
     $scope.showAdvanced = function () {
