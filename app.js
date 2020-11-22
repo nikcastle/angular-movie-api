4910d17324d028be71a4903862c60f07ac47b42b
Lady Stock <nikki.castlea@gmail.com>
2020-11-21 20:21:53 -0700

    added image to homepage

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.controller('myCtrl', function ($scope, $http) {
               
     $scope.apiCall = function(movieTitle) {
         // Search for a single movie
+        $(".homepage").hide();
+
         $http.get("http://www.omdbapi.com/?t=" + movieTitle + "&plot=full&apikey=24dbfc1d&")
             .then(function(result){
             $scope.details = result.data
