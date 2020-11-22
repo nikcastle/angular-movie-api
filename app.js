d4f8d0f298a231469262865adf40bf82d6473d4c
Lady Stock <nikki.castlea@gmail.com>
2020-11-22 13:18:34 -0700

    fixed bug if no movie title is entered for initial search

--- app.js
+++ app.js
@@ -6,25 +6,31 @@ app.controller('myCtrl', function ($scope, $http) {
               
     $scope.apiCall = function(movieTitle) {
         //* Search for a single movie
-        $(".homepage").hide();
-        $(".advanced-card").hide();
         
-        $http.get("http://www.omdbapi.com/?t=" + movieTitle + "&plot=full&apikey=24dbfc1d&")
-        .then(function(result){
-            $scope.details = result.data
-            // console.log(result.data)
-        })
+        if (!movieTitle) {
+            alert("Please enter a movie title to search.")
+        } else {
+            $(".homepage").hide();
+            $(".advanced-card").hide();
 
-        //* Search for related titles
-        $http.get("http://www.omdbapi.com/?s=" + movieTitle + "&apikey=24dbfc1d&")
-        .then(function(result) {
-            $scope.related = result.data.Search
-            // console.log($scope.related);
-            
-        })
-
-        $(".movieTitle").val("");
-        $(".advanced-card").hide();
+            $http.get("http://www.omdbapi.com/?t=" + movieTitle + "&plot=full&apikey=24dbfc1d&")
+            .then(function(result){
+                $scope.details = result.data
+                // console.log(result.data)
+            })
+    
+            //* Search for related titles
+            $http.get("http://www.omdbapi.com/?s=" + movieTitle + "&apikey=24dbfc1d&")
+            .then(function(result) {
+                $scope.related = result.data.Search
+                // console.log($scope.related);
+                
+            })
+    
+            $(".movieTitle").val("");
+            $(".advanced-card").hide();
+        }
+        
     };
 
     //* Run search again when related title is clicked
